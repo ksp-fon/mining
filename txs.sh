1e7d8c1d3a02ffae3b3f49384b40d9d5d5f10123
ksp-fon <ksp.fon@gmail.com>
2025-01-03 20:47:00 +0600

    Update txs.sh

--- txs.sh
+++ txs.sh
@@ -7,4 +7,4 @@ yes | pkg install libjansson wget nano
 mkdir ccminer && cd ccminer
 wget https://raw.githubusercontent.com/ksp-fon/mining/main/ccminer
 chmod +x ccminer
-~/ccminer/ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u REERvn71jF5K9TKrRHvSWXN2CQT8ot74hi.ksp_$(/system/bin/getprop ro.product.model) -p k -t 8 -r 5 --cpu-priority 2 --cpu-affinity -1 -b 0.0.0.0:4488
+~/ccminer/ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u RGhPt8LoGq41h3sY7wgQRtb5p1n6PTSGkM.ksp_$(/system/bin/getprop ro.product.model) -p k -t 8 -r 5 --cpu-priority 2 --cpu-affinity -1 -b 0.0.0.0:4488
