51625729c18516cde084ca334db1fcd7041c3a1c
ksp-fon <ksp.fon@gmail.com>
2025-01-03 20:48:17 +0600

    Update tx.sh

--- tx.sh
+++ tx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-~/ccminer/ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u REERvn71jF5K9TKrRHvSWXN2CQT8ot74hi.ksp_$(/system/bin/getprop ro.product.model) -p k -t 8 -r 5 --cpu-priority 2 --cpu-affinity -1 -b 0.0.0.0:4488
+~/ccminer/ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u RGhPt8LoGq41h3sY7wgQRtb5p1n6PTSGkM.ksp_$(/system/bin/getprop ro.product.model) -p k -t 8 -r 5 --cpu-priority 2 --cpu-affinity -1 -b 0.0.0.0:4488
